131a8420594a59d03bb1a1d8fd1dfab73644e501
Roman Fursov <contact@frsv.me>
2017-12-06 17:39:39 +0300

    Member-Clan circular reference. Add tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,7 @@
 def pytest_configure(config):
     from gqlclans.logic import PapiRequestSession
     from tests.requestsnapshot import FileSnapshotStore, SnapshotAdapter
-    s_adaptor = SnapshotAdapter(FileSnapshotStore('tests/testsnapshots'))
+    s_adaptor = SnapshotAdapter(FileSnapshotStore('tests/testsnapshots', True))
 
     PapiRequestSession.adapters['https://'] = s_adaptor
 
