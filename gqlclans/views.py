97735863acb3bc4e85f19a3c6e7d96727f0f35c0
Roman Fursov <contact@frsv.me>
2018-02-06 18:31:06 +0300

    Add headers to response to support react relay applications

--- gqlclans/views.py
+++ gqlclans/views.py
@@ -13,3 +13,19 @@ class ClansGraphQLView(BaseGraphQLView):
             'data_loaders': DataLoaders,
         })
         return context
+
+    def process_preflight(self, request):
+        response = super().process_preflight(request)
+        accepted_headers = ('Content-Type',)
+        if response.status == 200:
+            response.headers.update({
+                'Access-Control-Allow-Headers': ', '.join(accepted_headers),
+            })
+        return response
+
+    async def __call__(self, request):
+        response = await super().__call__(request)
+        response.headers.update({
+            'Access-Control-Allow-Origin': '*',
+        })
+        return response
